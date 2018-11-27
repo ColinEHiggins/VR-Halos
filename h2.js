b61341105ce54804296ca74a60c99d8817ab8daf
Enrique Jimenez <ej5997@gmail.com>
2018-11-27 18:30:52 -0500

    Added ambient music and background particles.

--- h2.js
+++ h2.js
@@ -9,6 +9,23 @@ var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 camera.position.z = 4;
 
+// create an AudioListener and add it to the camera
+var listener = new THREE.AudioListener();
+camera.add( listener );
+
+// create a global audio source
+var sound = new THREE.Audio( listener );
+
+// load a sound and set it as the Audio object's buffer
+// adds ambient music to the scene
+var audioLoader = new THREE.AudioLoader();
+audioLoader.load( 'ambient.wav', function( buffer ) {
+	sound.setBuffer( buffer );
+	sound.setLoop( true );
+	sound.setVolume( 0.05 );
+	sound.play();
+});
+
 // Create a renderer with Antialiasing
 var renderer = new THREE.WebGLRenderer({antialias:true});
 
@@ -25,7 +42,7 @@ document.body.appendChild( renderer.domElement );
 // FUN STARTS HERE
 // ------------------------------------------------
 
-// Create a Cube Mesh with basic material
+// Creates and textures 3 halos
 var texture = new THREE.TextureLoader().load( 'texture1.jpg' );
 var geometry = new THREE.TorusGeometry( 2.2, 0.4, 8, 100 );
 var material = new THREE.MeshBasicMaterial( { map: texture } );
@@ -41,11 +58,31 @@ var geometry3 = new THREE.TorusGeometry( 0.5, 0.2, 8, 100 );
 var material3 = new THREE.MeshBasicMaterial( { map: texture3 } );
 var torus3 = new THREE.Mesh( geometry3, material3 ); 
 
-// Add halos to Scene
+// generates background particles
+var material4 = new THREE.PointsMaterial({ color: 0xffffff, size: 1, sizeAttenuation: false });
+var geometry4 = new THREE.Geometry();
+var x, y, z;
+var i;
+for (i = 0; i < 15000; i++) 
+{
+      x = (Math.random() * 1500) - 750;
+      y = (Math.random() * 1500) - 750;
+      z = (Math.random() * 1500) - 750;
+      
+      geometry4.vertices.push(new THREE.Vector3(x, y, z));
+}
+    
+    var pointCloud = new THREE.Points(geometry4, material4);
+
+// Add halos and particles to scene
 scene.add( torus );
 scene.add( torus2 );
 scene.add( torus3 ); 
+scene.add(pointCloud);
 
+
+// checks to see if mouse position overlaps with any of the points contained by 
+// any of the halos
 document.addEventListener( 'mousedown', onDocumentMouseDown, false );
 
 var projector = new THREE.Projector();
